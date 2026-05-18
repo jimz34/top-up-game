04b54a1d4ca807ce902d072a24cd88e709cec4a2
jimz34 <testwkwk21@gmail.com>
2026-05-18 15:48:19 +0700

    Add supabase/migrations/20260518075056_e27e5659-2036-4eef-aa12-ec59d63e4858.sql

---
+++ supabase/migrations/20260518075056_e27e5659-2036-4eef-aa12-ec59d63e4858.sql
@@ -0,0 +1,5 @@
+
+alter function public.touch_updated_at() set search_path = public;
+revoke execute on function public.handle_new_user() from public, anon, authenticated;
+revoke execute on function public.touch_updated_at() from public, anon, authenticated;
+revoke execute on function public.has_role(uuid, app_role) from anon;
